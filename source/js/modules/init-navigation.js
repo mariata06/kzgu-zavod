dfaed4eb5678b481e12cc55b0be7afe9a9b23b87
mariata06 <slinkywizard@mail.ru>
2023-05-07 20:46:15 +0300

    Add footer, images for index.html

--- source/js/modules/init-navigation.js
+++ source/js/modules/init-navigation.js
@@ -1,8 +1,20 @@
 const initNavigation = () => {
 
   let navLinks = document.querySelectorAll('.main-nav__link');
-  navLinks.forEach(() => {
-    navLinks[0].classList.add('is-active');
+  // navLinks[0].classList.add('is-active');
+  navLinks.forEach((item) => {
+    // navLinks[0].classList.add('is-active');
+
+    item.addEventListener('click', () => {
+      navLinks[0].classList.remove('is-active');
+      // if (item.classList.contains('is-active')) {
+      //   item.classList.remove('is-active');
+      // } else {
+      //   item.classList.add('is-active');
+      // }
+      item.classList.add('is-active');
+      // navLinks[0].classList.remove('is-active');
+    });
   });
 };
 
